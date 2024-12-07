747d6c3a3d134e01eebba12a10177f7235417bb9
PRINCE BHADANIA <princebhadania007@gmail.com>
2024-12-07 22:32:47 +0530

    41_file.c

--- C Theory/41_file.c
+++ C Theory/41_file.c
@@ -3,7 +3,7 @@
 int main()
 {
     FILE *ptr;
-    ptr = fopen("harry.txt", "r");
+    ptr = fopen("Prince.txt", "r");
     int num;
     fscanf(ptr, "%d", &num);
     printf("The value of num is %d \n", num);
